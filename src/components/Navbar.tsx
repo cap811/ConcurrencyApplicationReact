0f337c17f52075bb81b08d7f32cd5488f3c7901b
cap811 <58876414+cap811@users.noreply.github.com>
2022-03-13 13:57:00 +0200

    Navigation bar and additional components, assets and directories added.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return(
+        <div>
+            <ul>
+                <li>VALUE TOWN</li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
